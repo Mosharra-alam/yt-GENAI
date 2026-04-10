bd58ae817ce8dc659dd0d6b297a023a69697d3bb
Mosharraf alam <mosharrafalam695@gmail.com>
2026-04-10 11:18:02 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
-
+import { RouterProvider } from "react-router"
+ import { router } from "./app.routes.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
+    
   )
 }
 
