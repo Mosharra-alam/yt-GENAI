0fb2ccac5dc7ed01639b6557504c210303a70753
Mosharraf alam <mosharrafalam695@gmail.com>
2026-04-13 00:46:46 +0530

    updates

--- src/features/auth/hooks/useAuth.js
+++ src/features/auth/hooks/useAuth.js
@@ -3,7 +3,7 @@ import  {useContext } from "react";
 
 import { AuthContext } from "../auth.context";
 
-
+    import { login, register, logout, getMe} from "../services/auth.api";   
 
 
 
@@ -11,5 +11,64 @@ export const useAuth = () => {
          const context = useContext(AuthContext)
 
          const { user, setUser, loading, setLoading } = context
+
+
+         const handleLogin = async ({email,password}) => {
+            setLoading(true)
+
+
+            try{
+            const date = await login({email,password})
+            setUser(data.user)
+            setLoading(false)
+         } catch (error) {
+
+
+          } finally {  
+          
+          setLoading(false)
+         }
+
+         }
+
+            const handleRegister = async ({username,email,password}) => {
+                setLoading(true)
+
+
+                try{
+                const data = await register({username,email,password})
+                setUser(data.user)
+                } catch (error) {
+
+
+                } finally {
+                setLoading(false) 
+                }  
+            }
+
+
+
+
+         const handleLogout = async () => {
+            setLoading(true)
+
+
+            try{
+            const data = await logout()
+            setUser(null)
+            } catch (error) {
+
+            } finally {
+            setLoading(false) 
+            }     
+         } 
+         
+         return {
+            user,
+            loading,
+            handleLogin,
+            handleRegister,
+            handleLogout
+         }
 
 }
