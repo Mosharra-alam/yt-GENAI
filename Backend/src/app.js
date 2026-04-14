4bbd8ac7a2f120bc1c2aea789a70b383cf1c002e
Mosharraf alam <mosharrafalam695@gmail.com>
2026-04-15 02:14:04 +0530

    updates

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,13 +9,13 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin: "http://localhost:5173",
+     origin:"http://localhost:5173",
      credentials: true
 }))
 
 
  /* required all the routes here */
-const authRouter = require("./routes/auth.routes.js")
+const authRouter = require("./routes/auth.routes")
 
 /* use the routes here */
 app.use("/api/auth", authRouter)
